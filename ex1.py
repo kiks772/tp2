ba47291008d35036f6849a927f7122842a36ac0d
MORAND Killian <12201383@crit-xubuntu>
2023-10-02 11:26:43 +0200

    creation ex 1 affiche v1

---
+++ ex1.py
@@ -0,0 +1,42 @@
+import unittest
+
+class FizzBuzz:
+    def __init__(self):
+        self.result = ""
+
+    def affiche(self):
+        for i in range(1, 101):
+            if i % 15 == 0:
+                self.result += "FrisBee"
+            elif i % 3 == 0:
+                self.result += "Fizz"
+            elif i % 5 == 0:
+                self.result += "Buzz"
+            else:
+                self.result += str(i)
+        return self.result
+
+    def verifier(self):
+        if self.result:
+            return self.result[:3], self.result[-3:]
+        else:
+            raise ValueError("Veuillez d'abord exécuter la méthode affiche().")
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_affiche(self):
+        fb = FizzBuzz()
+        result = fb.affiche()
+        self.assertTrue(result.startswith("12Fizz"))
+        self.assertTrue(result.endswith("98FizzBuzz"))
+
+    def test_verifier(self):
+        fb = FizzBuzz()
+        fb.affiche()
+        debut, fin = fb.verifier()
+        self.assertEqual(debut, "12F")
+        self.assertEqual(fin, "uzz")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
